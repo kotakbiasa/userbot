15ac997c7bbd2cfc34770248a3adde2660f4afa3
N/A <N/A>
2025-09-15 03:25:27 +0700

    chore(selfbot.modules.network): answer before execute

--- selfbot/modules/network.py
+++ selfbot/modules/network.py
@@ -57,6 +57,7 @@ class Network(Module):
         if event.from_user.id != self.client.app.me.id:
             return await event.answer("Who are You?", show_alert=True, cache_time=900)
 
+        await event.answer(cache_time=0)
         await self.edit(event)
 
     async def edit(self, event: Update) -> None:
