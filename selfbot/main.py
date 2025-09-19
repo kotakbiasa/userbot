70116f2e0a8e7afabb2c770cf07a8b1633b424fd
Troublescope <121922135+troublescope@users.noreply.github.com>
2025-09-19 15:15:21 +0700

    fix(selfbot/main.py): rename config keys . my fault
    Signed-off-by: Troublescope <121922135+troublescope@users.noreply.github.com>

--- selfbot/main.py
+++ selfbot/main.py
@@ -18,7 +18,7 @@ for lib in ["pyrogram", "httpx"]:
 
 def config() -> dict:
     config = {k.lower(): v for k, v in (dotenv_values() or os.environ).items()}
-    if not (config.get("app_string_session") or config.get("bot_string_session")):
+    if not (config.get("app_session_string") or config.get("bot_session_string")):
         raise SystemExit("Missing app_string_session or bot_string_session")
 
     return config
