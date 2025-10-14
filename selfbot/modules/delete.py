ec35e26fd935b115932085996dea5c7b359049e0
beepsound <219347502+beepsound@users.noreply.github.com>
2025-10-15 03:34:24 +0700

    1

--- selfbot/modules/delete.py
+++ selfbot/modules/delete.py
@@ -1,7 +1,6 @@
 import re
 
 from pyrogram import filters
-from pyrogram.errors import RPCError
 from pyrogram.types import Message
 
 from selfbot import listener
@@ -18,24 +17,11 @@ class Delete(Module):
 
     @listener.handler(filters.regex(pattern), 1)
     async def on_message_out(self, event: Message) -> None:
-        (mid,) = pattern.match(event.content).groups()
-        if event.reply_to_message_id:
-            mid = event.reply_to_message_id
-
-        await event.delete(True)
-
+        ids, (mid,) = [event.id], pattern.match(event.content).groups()
         if mid:
-            try:
-                msg = await event._client.get_messages(event.chat.id, int(mid))
-            except RPCError:
-                pass
-            else:
-                ids = [msg.id]
-                if (
-                    msg.reply_to_message_id
-                    and msg.reply_to_message.from_user
-                    and msg.reply_to_message.from_user.is_self
-                ):
-                    ids.append(msg.reply_to_message_id)
+            ids.append(int(mid))
 
-                await event._client.delete_messages(event.chat.id, ids)
+        if event.reply_to_message_id:
+            ids.append(event.reply_to_message_id)
+
+        await event._client.delete_messages(event.chat.id, ids)
