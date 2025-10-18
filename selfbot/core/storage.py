2aa96bfdcab0dacec37f347dc56b412881549c9f
beepsound <219347502+beepsound@users.noreply.github.com>
2025-10-18 18:30:29 +0700

    test

--- selfbot/core/storage.py
+++ selfbot/core/storage.py
@@ -65,8 +65,9 @@ def get_input_peer(peer_id: int, access_hash: int, peer_type: str) -> InputPeer:
 
 
 class PostgreStorage(Storage):
-    def __init__(self, pool: Pool) -> None:
-        super().__init__(self)
+    def __init__(self, name: str, pool: Pool) -> None:
+        super().__init__(name)
+        self.name = name
         self.pool = pool
 
     async def open(self) -> None:
